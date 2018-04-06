1ec326eb62153aa4cb61235c9259f31c5edd3b7b
Sean Batzel <romulus108@protonmail.com>
2018-04-06 12:45:48 -0400

    Made changes to the database file and the related section of the paper.

--- blockchain_message/src/database/database.rs
+++ blockchain_message/src/database/database.rs
@@ -1,2 +1,10 @@
 pub struct Database {
+    contacts: Vec<Contact>,
+    messages: Vec<Message>
+}
+
+impl Database {
+    pub fn insert(Message msg) -> i32 {}
+    pub fn delete(i32 msgid) -> i32 {}
+    pub fn read(i32 msgid) -> message {}
 }
