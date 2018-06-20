27da124eed694b71ecde6d67c633ff7324a52de9
Sean Batzel <romulus108@protonmail.com>
2018-06-19 21:15:42 -0400

    Started working on the client.

---
+++ client/cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+blockchain_message simple client implementation.
+
+This program is distributed with ABSOLUTELY NO WARRANTY. A copy of the GNU Public License should have been distributed
+along with the source code. If not, the full text can be found at https://www.gnu.org/licenses/gpl-3.0.txt.
+"""
+
+__version__ = "0.1.0"
+
+__author__ = "Sean Batzel"
+__email__ = "romulus108@protonmail.com"
+__license__ = "GPL"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
